83f6c81b73366c28869e19f249b2e97f737fa1d8
dev-zam <zamsyh.work@gmail.com>
2021-03-01 14:14:13 +0700

    Update Models Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
+use App\Models\Category;
 
 class Product extends Model
 {
+
+
     use HasFactory;
     protected $connection = 'mongodb';
+    protected $fillable = ['id', 'name', 'slug', 'category_id', 'description', 'image', 'price', 'weight', 'status'];
+
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+
+
 }
