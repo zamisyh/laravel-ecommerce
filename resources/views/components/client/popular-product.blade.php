ca14a9b87dc6cce55fd0ee13215b91078922d9d9
dev-zam <zamsyh.work@gmail.com>
2021-03-08 20:12:24 +0700

    Create component popular products

---
+++ resources/views/components/client/popular-product.blade.php
@@ -0,0 +1,49 @@
+<div style="margin-top: -10%">
+      <!--? Popular Items Start -->
+      <div class="popular-items section-padding30">
+        <div class="container">
+            <!-- Section tittle -->
+            <div class="row justify-content-center">
+                <div class="col-xl-7 col-lg-8 col-md-10">
+                    <div class="section-tittle mb-70 text-center">
+                        <h2>Popular Items</h2>
+                        <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+               @foreach ($product['popularProduct'] as $item)
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-popular-items mb-50 text-center">
+                        <div class="popular-img">
+                            <img src="{{ asset('storage/images/product/' . $item->image) }}" alt="">
+                            <div class="img-cap">
+                                <span wire:click="addToCart({{$item->id}})">Add to cart</span>
+                                <input type="hidden" wire:model="product_id" value="{{ $item->id }}">
+                                
+                                
+                            </div>
+                            <div class="favorit-items">
+                                <li class="badge badge-primary" style="list-style: none">{{ $item->category->category }}</li>
+                            </div>
+                        </div>
+                        <div class="popular-caption">
+                            <h3><a href='{{ url("product/{$item->slug}") }}'>{{ $item->name }}</a></h3>
+                            <span>Rp. {{ number_format($item->price) }}</span>
+                        </div>
+                    </div>
+                </div>
+               @endforeach
+            </div>
+
+
+
+            <!-- Button -->
+            <div class="row justify-content-center">
+                <div class="room-btn pt-70">
+                    <a href="catagori.html" class="btn view-btn1">View More Products</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
