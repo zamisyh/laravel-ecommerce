a89f3cdcef92792cc06423167d84f63e30777166
dev-zam <zamsyh.work@gmail.com>
2021-03-08 21:06:38 +0700

    Update navbar

--- resources/views/livewire/client/components/navbar.blade.php
+++ resources/views/livewire/client/components/navbar.blade.php
@@ -16,27 +16,7 @@
                                     <li><a href="{{ route('client.home')}}" wire:click='$refresh' >Home</a></li>
                                     <li><a href="{{ route('client.shop') }}">shop</a></li>
                                     <li><a href="about.html">about</a></li>
-                                    <li class="hot"><a href="#">Latest</a>
-                                        <ul class="submenu">
-                                            <li><a href="shop.html"> Product list</a></li>
-                                            <li><a href="product_details.html"> Product Details</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="blog.html">Blog</a>
-                                        <ul class="submenu">
-                                            <li><a href="blog.html">Blog</a></li>
-                                            <li><a href="blog-details.html">Blog Details</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#">Pages</a>
-                                        <ul class="submenu">
-                                            <li><a href="login.html">Login</a></li>
-                                            <li><a href="cart.html">Cart</a></li>
-                                            <li><a href="elements.html">Element</a></li>
-                                            <li><a href="confirmation.html">Confirmation</a></li>
-                                            <li><a href="checkout.html">Product Checkout</a></li>
-                                        </ul>
-                                    </li>
+                                  
                                     <li><a href="contact.html">Contact</a></li>
                                 </ul>
                             </nav>
@@ -50,7 +30,7 @@
                                     </div>
                                 </li>
                                 <li> <a href="login.html"><span class="flaticon-user"></span></a></li>
-                                <li><a href="cart.html"><span class="flaticon-shopping-cart"></span></a> </li>
+                                <li><a href="cart.html"><span class="flaticon-shopping-cart"><span style="margin-left: -10px">1</span></span></a></li>
                             </ul>
                         </div>
                     </div>
