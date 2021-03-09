203f14518dd1add122e362ae5b2082bc343842f9
dev-zam <zamsyh.work@gmail.com>
2021-03-09 18:11:08 +0700

    Adding router cart in navbar

--- resources/views/livewire/client/components/navbar.blade.php
+++ resources/views/livewire/client/components/navbar.blade.php
@@ -16,7 +16,7 @@
                                     <li><a href="{{ route('client.home')}}" wire:click='$refresh' >Home</a></li>
                                     <li><a href="{{ route('client.shop') }}">shop</a></li>
                                     <li><a href="about.html">about</a></li>
-                                  
+
                                     <li><a href="contact.html">Contact</a></li>
                                 </ul>
                             </nav>
@@ -30,7 +30,7 @@
                                     </div>
                                 </li>
                                 <li> <a href="login.html"><span class="flaticon-user"></span></a></li>
-                                <li><a href="cart.html"><span class="flaticon-shopping-cart"><span style="margin-left: -10px">1</span></span></a></li>
+                                <li><a href='{{ route('client.cart') }}'><span class="flaticon-shopping-cart"><span style="margin-left: -10px">{{ $cartTotal }}</span></span></a></li>
                             </ul>
                         </div>
                     </div>
