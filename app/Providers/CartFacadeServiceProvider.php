5c6a92f5498b2412e08cd4ccba64c8375a974803
zamisyh <zamsyh.work@gmail.com>
2021-03-09 10:07:16 +0700

    app/Providers/CartFacadeServiceProvider: create

---
+++ app/Providers/CartFacadeServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Cart;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
+
+class CartFacadeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        App::bind('cart', function() {
+            return new Cart;
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
